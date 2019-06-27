b327527ea4c1f99fd1bcf0456c37495bdce48767
hayato yamada <yamabou@gmail.com>
2019-06-27 17:22:11 +0900

    バリデーションチェックを試行

---
+++ Models/Starship.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Starship
+{
+    [Required]
+    [StringLength(16, 
+        ErrorMessage = "Identifier too long (16 character limit).")]
+    public string Identifier { get; set; }
+
+    // Optional (no data annotations)
+    public string Description { get; set; }
+
+    [Required]
+    public string Classification { get; set; }
+
+    [Range(1, 100000, ErrorMessage = "Accommodation invalid (1-100000).")]
+    public int MaximumAccommodation { get; set; }
+
+    [Required]
+    [Range(typeof(bool), "true", "true", 
+        ErrorMessage = "Form disallowed for unapproved ships.")]
+    public bool IsValidatedDesign { get; set; }
+
+    [Required]
+    public DateTime ProductionDate { get; set; }
+}
